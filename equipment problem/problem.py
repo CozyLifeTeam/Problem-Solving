0c78b6fc9fddb25a9bff282b33dc9faacbe28777
tester <cozylife_tester@doit.am>
2023-08-08 14:48:08 +0800

    problem

---
+++ equipment problem/problem.py
@@ -0,0 +1,61 @@
+1. 插座无法打开,指示灯没有闪烁
+    The socket cannot be opened and the indicator light is not flashing.
+
+解决办法: http://api-cn.doiting.com/dynamic/cozylife/faq/pages/faqDetail/faqDetail?faq_id=71
+solution   http://api-cn.doiting.com/dynamic/cozylife/faq/pages/faqDetail/faqDetail?faq_id=71
+
+2.设备的颜色/亮度/色温控制出现异常
+   Abnormal color/brightness/color temperature control of the device.
+
+解决办法: http://api-cn.doiting.com/dynamic/cozylife/faq/pages/faqDetail/faqDetail?faq_id=72
+solution  http://api-cn.doiting.com/dynamic/cozylife/faq/pages/faqDetail/faqDetail?faq_id=72
+
+3.设备显示离线
+   Device display offline.
+解决办法: http://api-cn.doiting.com/dynamic/cozylife/faq/pages/faqDetail/faqDetail?faq_id=73
+solution  http://api-cn.doiting.com/dynamic/cozylife/faq/pages/faqDetail/faqDetail?faq_id=73
+
+4.设备怎么连不上网？
+   Why can't the device connect to the internet?
+解决办法: http://api-cn.doiting.com/dynamic/cozylife/faq/pages/faqDetail/faqDetail?faq_id=74
+solution  http://api-cn.doiting.com/dynamic/cozylife/faq/pages/faqDetail/faqDetail?faq_id=74
+
+5.为什么家中的设备出现自行开关或闪烁的现象？
+   Why do devices at home experience self switching or flashing?
+解决办法: http://api-cn.doiting.com/dynamic/cozylife/faq/pages/faqDetail/faqDetail?faq_id=75
+solution  http://api-cn.doiting.com/dynamic/cozylife/faq/pages/faqDetail/faqDetail?faq_id=75
+
+6.设备自动消失怎么回事?
+   What's wrong with the device disappearing automatically?
+解决办法: http://api-cn.doiting.com/dynamic/cozylife/faq/pages/faqDetail/faqDetail?faq_id=76
+solution  http://api-cn.doiting.com/dynamic/cozylife/faq/pages/faqDetail/faqDetail?faq_id=76
+
+7.设备怎么联网
+  How to connect devices to the internet
+解决办法: http://api-cn.doiting.com/dynamic/cozylife/faq/pages/faqDetail/faqDetail?faq_id=77
+solution  http://api-cn.doiting.com/dynamic/cozylife/faq/pages/faqDetail/faqDetail?faq_id=77
+
+8.APP无法远程控制设备，控制后不生效
+The app cannot remotely control the device and will not take effect after control
+
+解决办法: http://api-cn.doiting.com/dynamic/cozylife/faq/pages/faqDetail/faqDetail?faq_id=78
+solution  http://api-cn.doiting.com/dynamic/cozylife/faq/pages/faqDetail/faqDetail?faq_id=78
+
+
+9.设备联网后，在同一个地点可以控制，但是手机换别的地址或者别的网络就无法远程控制了
+  After the device is connected to the network, it can be controlled in the same location, 
+  but it cannot be remotely controlled by changing the phone to another address or network
+
+解决办法: http://api-cn.doiting.com/dynamic/cozylife/faq/pages/faqDetail/faqDetail?faq_id=79
+solution  http://api-cn.doiting.com/dynamic/cozylife/faq/pages/faqDetail/faqDetail?faq_id=79
+
+
+10设备状态无法与APP上显示的状态同步
+The device status cannot be synchronized with the status displayed on the APP
+
+解决办法: http://api-cn.doiting.com/dynamic/cozylife/faq/pages/faqDetail/faqDetail?faq_id=80
+solution  http://api-cn.doiting.com/dynamic/cozylife/faq/pages/faqDetail/faqDetail?faq_id=80
+
+
+
+
